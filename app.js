05450ca0237c5b3bb16f67794b0b25240a1b6987
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-28 15:30:58 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var cluster = require('cluster');
+var os = require('os');
+if(cluster.isMaster) {
+ var cpus = os.cpus().length
+ 
+ //start as many children as the number of CPUs
+ for (var i = 0; i < cpus; i++) { 
+ cluster.fork();
+ }
+
+cluster.on('exit', function(worker, code) {
+    if(code != 0 && !worker.suicide) {
+        console.log('Worker crashed. Starting a new worker');
+        cluster.fork();
+    }
+});
+
+} else {
+ require('./index'); 
+}
