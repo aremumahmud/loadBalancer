d7b5778deb865b05f817ba6be340ba24078855db
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-28 14:00:46 +0100

    created index

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var DataStream = require("./DataStream")
+var fs = require("fs")
+var servers = fs.readFileSync("./servers.file").toString().split("\n")
+console.log(servers)
+var index = 0
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+require('http').createServer(function(req, res) {
+ if(req.url.indexOf("/Update") != 0){
+ if(!servers.length) {
+ res.writeHead(502);
+ return res.end('Bad gateway');
+ }
+ index = (index + 1) % servers.length;
+ console.log(index)//[4]
+ proxy.web(req, res, {target:servers[index]},(err)=>{
+   res.end("err")
+ });
+ }else{
+   var { url } = req
+   var query = url.slice(url.indexOf("?")+1)
+   var list = query.split("=")
+   servers.push(list[1])
+     var text = servers.join("\n")
+     var dataStream = new DataStream(text)
+     var writer = fs.createWriteStream("servers.file")
+     dataStream
+        .pipe(writer)
+        .on("end" ,()=>{
+          res.end("sucess")
+        })
+ }
+}).listen(8080, function() {console.log('Started');});
