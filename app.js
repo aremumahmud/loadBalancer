bf33b1191d478c741dff49443ba19a0838ef6acb
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-29 23:41:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ if(cluster.isMaster) {
  
  //start as many children as the number of CPUs
  for (var i = 0; i < cpus; i++) { 
- cluster.fork();
+    cluster.fork();
  }
 
 cluster.on('exit', function(worker, code) {
@@ -15,21 +15,6 @@ cluster.on('exit', function(worker, code) {
         cluster.fork();
     }
 });
-var http = require("http")
-http.createServer((req,res)=>{
-   var { url } = req
-   var query = url.slice(url.indexOf("?")+1)
-   var list = query.split("=")
-   if (list.length <= 2){
-       Object.keys(cluster.workers).forEach(function(id) {
-            cluster.workers[id].send(list[1]);
-       });
-       res.end("sent")
-   }else{
-       res.end("wrong params")
-   }
-
-  }).listen(process.env.PORT || 8080)
 
 } else {
  require('./index'); 
