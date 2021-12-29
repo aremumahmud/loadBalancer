6a1e7aa995cff5e60d246bab86d782aab405c2f1
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-30 00:02:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var cluster = require('cluster');
 var os = require('os');
 if(cluster.isMaster) {
- var cpus = 4//os.cpus().length
+ var cpus = os.cpus().length
  let worker;
  //start as many children as the number of CPUs
  for (var i = 0; i < cpus; i++) { 
