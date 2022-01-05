9da0324b27839f2c4bad74d9c1b710ca1ee928ff
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2022-01-05 23:35:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ if(req.url == "/favicon.ico"){
  }
  index = (index + 1) % servers.length;
  console.log(servers[index])//[4]
-req.headers.host = "mongodbservice1.herokuapp.com"
- proxy.web(req, res, {target:"mongodbservice1.herokuapp.com"},(err)=>{
+req.headers.host = "http://mongodbservice1.herokuapp.com"
+ proxy.web(req, res, {target:"http://mongodbservice1.herokuapp.com"},(err)=>{
    res.end("err")
 console.log(err)
  });
